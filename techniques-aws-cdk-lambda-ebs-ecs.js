48c1916cff66ed6bfa3912783e0fabcc31c349b9
mazina <mn.usyd@gmail.com>
2024-01-06 07:24:18 +1100

    changed syntax to ts

--- techniques-aws-cdk-lambda-ebs-ecs.js
+++ techniques-aws-cdk-lambda-ebs-ecs.js
@@ -2,6 +2,19 @@
 npm install -g aws-cdk
 cdk --version
 
+cdk init app --language=typescript
+
+my-cdk-app/
+|-- lib/
+|   |-- my-stack.js
+|   lambda
+|   |-- my-lambda.js
+|-- cdk.json
+|-- package.json
+
+cdk deploy
+cdk synth
+cdk deploy my-stack (if not in cdk.json)
 
 #stack.js
 
@@ -356,7 +369,8 @@ class EcsLambdaStack extends cdk.Stack {
 
     // Define your container details here
     const container = taskDefinition.addContainer('MyContainer', {
-      image: ecs.ContainerImage.fromRegistry('your-docker-image'),
+      //image: ecs.ContainerImage.fromRegistry('your-docker-image'),
+      image: ecs.ContainerImage.fromRegistry('php:8.2-cli),
     });
 
     // ECS Service
@@ -369,7 +383,7 @@ class EcsLambdaStack extends cdk.Stack {
     const handler = new lambda.Function(this, 'MyLambdaFunction', {
       runtime: lambda.Runtime.NODEJS_14_X,
       handler: 'index.handler',
-      code: lambda.Code.fromAsset('lambda-code'), // Path to your Lambda function code
+      code: lambda.Code.fromAsset('lambda'), // Path to your Lambda function code
       environment: {
         ECS_CLUSTER_NAME: cluster.clusterName,
         ECS_SERVICE_NAME: service.serviceName,
@@ -409,4 +423,63 @@ exports.handler = async (event, context) => {
   };
 };
 
+
+@ts syntax:
+
+import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+
+export class MazCdkEcsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  super(scope, id, props);
+
+  // SQS Queue
+  const queue = new sqs.Queue(this, 'MyQueue', {
+    visibilityTimeout: cdk.Duration.seconds(300),
+  });
+
+  // ECS Cluster
+  const cluster = new ecs.Cluster(this, 'MyCluster');
+
+  // ECS Task Definition
+  const taskDefinition = new ecs.FargateTaskDefinition(this, 'MyTaskDefinition');
+
+  // Define your container details here
+  const container = taskDefinition.addContainer('MyContainer', {
+    image: ecs.ContainerImage.fromRegistry('php:8.2-cli'),
+  });
+
+  // ECS Service
+  const service = new ecs.FargateService(this, 'MyService', {
+    cluster,
+    taskDefinition,
+  });
+
+  // Lambda Function
+  const handler = new lambda.Function(this, 'MyLambdaFunction', {
+    runtime: lambda.Runtime.NODEJS_14_X,
+    handler: 'index.handler',
+    code: lambda.Code.fromAsset('lambda-code'), // Path to your Lambda function code
+    environment: {
+      ECS_CLUSTER_NAME: cluster.clusterName,
+      ECS_SERVICE_NAME: service.serviceName,
+      SQS_QUEUE_URL: queue.queueUrl,
+    },
+  });
+
+  // Grant permissions for Lambda to receive messages from SQS
+  queue.grantConsumeMessages(handler);
+
+  // Schedule the Lambda function to run every week on Sundays
+  new events.Rule(this, 'WeeklySchedule', {
+  schedule: events.Schedule.cron({ minute: '0', hour: '0', day: 'SUN' }),
+targets: [new targets.LambdaFunction(handler)],
+});
+}
+}
 
