98847d4676cf275483e2deb86327c8a40416c77b
Maziar Navabi <mn.usyd@gmail.com>
2024-01-26 19:20:11 +1100

    Update techniques-pagination-mssql.sql using row numbers

--- techniques-pagination-mssql.sql
+++ techniques-pagination-mssql.sql
@@ -40,3 +40,29 @@ OFFSET (@PageSize * (@PageNumber - 1)) ROWS
 FETCH NEXT @PageSize ROWS ONLY;
 
 
+--third solution
+DECLARE @PageSize INT = /* your desired page size */;
+DECLARE @PageNumber INT = /* specify the page number you want to retrieve */;
+
+WITH OriginalResultSet AS (
+    SELECT column1, column2, ...
+    FROM table1
+    JOIN table2 ON table1.column = table2.column
+    JOIN table3 ON table2.column = table3.column
+    -- Add other joins, conditions, and ordering as needed
+),
+PagedResultSet AS (
+    SELECT 
+        column1, column2, ...,
+        ROW_NUMBER() OVER (ORDER BY some_column) AS RowNum,
+        COUNT(*) OVER () AS TotalRecords
+    FROM OriginalResultSet
+)
+SELECT 
+    column1, column2, ...,
+    TotalRecords
+FROM PagedResultSet
+WHERE RowNum BETWEEN (@PageSize * (@PageNumber - 1) + 1) AND (@PageSize * @PageNumber);
+
+
+
